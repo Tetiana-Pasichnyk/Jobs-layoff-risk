59110e92884010ef208c2e6f1e683028b93593f6
pasichnykTetiana-create <pasichnyktetiana.de@gmail.com>
2026-07-01 16:39:56 +0200

    änfern settingsmethode

--- src/database/settings.py
+++ src/database/settings.py
@@ -1,6 +1,10 @@
 import os
 
-from src.paths import CSV_PATH, DATA_DIR, OUTPUT_DIR
+from dotenv import load_dotenv
+
+from src.paths import CSV_PATH, DATA_DIR, OUTPUT_DIR, PROJECT_ROOT
+
+load_dotenv(PROJECT_ROOT / ".env")
 
 DB_PASSWORD = os.getenv("DB_PASSWORD", "root")
 DB_PORT = int(os.getenv("DB_PORT", "8889"))
